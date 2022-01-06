0ff89d7d54007fdb7b44120f9eb09e05f8b0bdf8
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-01-05 23:31:10 -0300

    Visual finished

---
+++ script.js
@@ -0,0 +1,10 @@
+// https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop
+// https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_global_draggable
+// https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2
+// https://www.w3schools.com/html/html5_draganddrop.asp
+
+
+
+
+
+
