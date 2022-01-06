03cc263598b93874305613946bd36cf64f93be19
Ramon <ramon_max1986@hotmail.com>
2022-01-06 17:35:37 -0300

    Reset

--- script.js
+++ script.js
@@ -4,6 +4,30 @@
 // https://www.w3schools.com/html/html5_draganddrop.asp
 
 
+function allowDrop(item){
+    item.preventDefault()
+}
+
+function drag(item){
+    item.dataTransfer.setData("Text", item.target.id);
+}
+
+function drop(item){
+    //Call preventDefault() to prevent the browser default handling of the data (default is open as link on drop)
+    var square = item.dataTransfer.getData("Text");
+    item.target.appendChild(document.getElementById(square))
+    item.preventDefault();
+}
+
+
+function reset(){
+    window.location.reload();
+}
+
+
+
+
+
 
 
 
