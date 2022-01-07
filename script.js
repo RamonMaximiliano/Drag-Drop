6ad7ca005abbff226c73c5e9981f5761d029293f
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-01-07 01:16:24 -0300

    Validating

--- script.js
+++ script.js
@@ -4,6 +4,11 @@
 // https://www.w3schools.com/html/html5_draganddrop.asp
 
 
+
+//Validation
+//https://www.javascripttutorial.net/javascript-dom/javascript-get-child-element/
+//https://www.codegrepper.com/code-examples/javascript/javascript+get+specific+child+element
+
 function allowDrop(item){
     item.preventDefault()
 }
@@ -12,13 +17,12 @@ function drag(item){
     item.dataTransfer.setData("Text", item.target.id);
 }
 
-function drop(item){
+function dropSquare(item){
     //Call preventDefault() to prevent the browser default handling of the data (default is open as link on drop)
     var square = item.dataTransfer.getData("Text");
     item.target.appendChild(document.getElementById(square))
     item.preventDefault();
 }
-
 
 function reset(){
     window.location.reload();
